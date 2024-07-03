a3baba5669b63da270b9fbbb5008edd0f28b4a2a
Alfredo Hinojosa <alfhh4@gmail.com>
2024-07-02 19:46:33 -0700

    GitGenie bump version

--- Formula/git-genie.rb
+++ Formula/git-genie.rb
@@ -1,7 +1,7 @@
 class GitGenie < Formula
   desc "A command-line tool to help you manage your GitHub repositories"
   homepage "https://gitgenie.io/"
-  url "https://github.com/alfhh/git-genie/releases/download/v0.1.5-test/git-genie.phar"
+  url "https://github.com/alfhh/git-genie/releases/download/v0.1.6-test/git-genie.phar"
   sha256 "daa1b0c3873d7fe401a75075398d85fa17dcb29235aa656c42ef4a9fb0ec8a81"
   license "MIT"
 
