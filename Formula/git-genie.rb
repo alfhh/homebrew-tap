4065849be393c6c200d18500a15072bc8a6d0cf2
Alfredo Hinojosa <alfhh4@gmail.com>
2024-07-11 00:30:41 -0700

    Updated Git Genie has to v1.0.0

--- Formula/git-genie.rb
+++ Formula/git-genie.rb
@@ -2,7 +2,7 @@ class GitGenie < Formula
   desc "A command-line tool to help you manage your GitHub repositories"
   homepage "https://gitgenie.io/"
   url "https://github.com/alfhh/git-genie/releases/download/v0.1.6-test/git-genie.phar"
-  sha256 "5e3b44364e49eb2162f17326964d8877d3ec75b4fa8a3eaf86b254f120bcfbe6"
+  sha256 "6809343ea2629ed54b950be3786adf375a47a632075e476dc049a40f684c1cc5"
   license "MIT"
 
   livecheck do
